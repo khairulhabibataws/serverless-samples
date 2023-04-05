48277e93f5bc79ef9d336abb79b400dd7acbef66
Khairul Habib <mkhabib@amazon.id>
2023-04-05 22:23:02 +0700

    add more use case based on python use case

--- serverless-graphql-api/java-appsync-sam/src/test/java/com/example/MockTest.java
+++ serverless-graphql-api/java-appsync-sam/src/test/java/com/example/MockTest.java
@@ -84,6 +84,7 @@ public class MockTest {
         String context = "{\n            \"arguments\":\n                {\n                },\n            \"result\": {\n                    \"items\": [\n                        {\n                            \"locationid\": \"0\",\n                            \"imageUrl\": \"url\",\n                            \"name\": \"name\",\n                            \"description\": \"description\",\n                            \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n                        },\n                        {\n                            \"locationid\": \"2\",\n                            \"imageUrl\": \"url2\",\n                            \"name\": \"name2\",\n                            \"description\": \"description2\",\n                            \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n                        },\n                        {\n                            \"locationid\": \"1\",\n                            \"imageUrl\": \"url1\",\n                            \"name\": \"name1\",\n                            \"description\": \"description1\",\n                            \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n                        }\n                    ],\n                    \"scannedCount\": 3\n            }\n        }";
         ObjectMapper mapper = new ObjectMapper();
         JsonNode contextMap = mapper.readTree(context);
+        JsonNode contextMapItems = contextMap.get("result").get("items");
         // Test request mapping template
         EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
             .template(getTemplateToString("get_locations_request.vtl"))
@@ -93,41 +94,23 @@ public class MockTest {
         Assertions.assertNotNull(templateResponseRequest.evaluationResult());
         
         JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
-        
         Assertions.assertEquals("Scan", resultMapRequest.get("operation").asText());
-        
-    }
-
-    @Test
-    public void testDeleteLocationResolver() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode contextMap = mapper.readTree(locationContext);
-
-        // Test request mapping template
-        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
-            .template(getTemplateToString("delete_location_request.vtl"))
-            .context(locationContext)
-            .build();
-        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
-        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
-        
-        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
-        
-        Assertions.assertEquals("DeleteItem", resultMapRequest.get("operation").asText());
-        Assertions.assertEquals(contextMap.get("arguments").get("locationid"), resultMapRequest.get("key").get("locationid").get("S"));
 
         // Test response mapping response
         EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
-            .template(getTemplateToString("delete_location_response.vtl"))
-            .context(locationContext)
+            .template(getTemplateToString("get_locations_response.vtl"))
+            .context(context)
             .build();
         EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
         Assertions.assertNotNull(templateResponseResponse.evaluationResult());
 
         JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        int i=0;
         for (JsonNode jsonNode : resultMapResponse) {
-            Assertions.assertEquals(contextMap.get("result").get(jsonNode.asText()), resultMapResponse.get(jsonNode.asText()));
+            Assertions.assertEquals(contextMapItems.get(i).get("locationid").asText(), jsonNode.get("locationid").asText());
+            i++;
         }
+        
     }
 
     @Test
@@ -205,6 +188,113 @@ public class MockTest {
     }
 
     @Test
+    public void testDeleteLocationResolver() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode contextMap = mapper.readTree(locationContext);
+
+        // Test request mapping template
+        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("delete_location_request.vtl"))
+            .context(locationContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
+        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
+        
+        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
+        
+        Assertions.assertEquals("DeleteItem", resultMapRequest.get("operation").asText());
+        Assertions.assertEquals(contextMap.get("arguments").get("locationid"), resultMapRequest.get("key").get("locationid").get("S"));
+
+        // Test response mapping response
+        EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("delete_location_response.vtl"))
+            .context(locationContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
+        Assertions.assertNotNull(templateResponseResponse.evaluationResult());
+
+        JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        for (JsonNode jsonNode : resultMapResponse) {
+            Assertions.assertEquals(contextMap.get("result").get(jsonNode.asText()), resultMapResponse.get(jsonNode.asText()));
+        }
+    }
+
+    @Test
+    public void testGetSingleResourceResolver() throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode contextMap = mapper.readTree(resourceContext);
+
+        // Test request mapping template
+        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_resource_request.vtl"))
+            .context(resourceContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
+        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
+        
+        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
+        Assertions.assertEquals("GetItem", resultMapRequest.get("operation").asText());
+        Assertions.assertEquals(contextMap.get("arguments").get("resourceid"), resultMapRequest.get("key").get("resourceid").get("S"));
+
+        // Test response mapping response
+        EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_resource_response.vtl"))
+            .context(resourceContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
+        Assertions.assertNotNull(templateResponseResponse.evaluationResult());
+
+        JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        for (JsonNode jsonNode : resultMapResponse) {
+            Assertions.assertEquals(contextMap.get("result").get(jsonNode.asText()), resultMapResponse.get(jsonNode.asText()));
+        }
+    }
+
+    @Test
+    public void testGetResourceForLocationResolver() throws IOException {
+        String context = "{\n            \"arguments\":\n                {\n                    \"locationid\": \"abcdefghij\"\n                },\n            \"result\": {\n                    \"items\": [\n                        {\n                            \"resourceid\": \"0\",\n                            \"locationid\": \"abcdefghij\",\n                            \"name\": \"Resource Name 0\",\n                            \"description\": \"Resource Description 0\",\n                            \"type\": \"Resource Type 0\",\n                            \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n                        },\n                        {\n                            \"resourceid\": \"1\",\n                            \"locationid\": \"abcdefghij\",\n                            \"name\": \"Resource Name 1\",\n                            \"description\": \"Resource Description 1\",\n                            \"type\": \"Resource Type 1\",\n                            \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n                        }\n                    ],\n                    \"scannedCount\": 2\n            }\n        }";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode contextMap = mapper.readTree(context);
+        JsonNode contextMapItems = contextMap.get("result").get("items");
+        System.out.println("contextMapItems " + contextMapItems);
+
+        // Test request mapping template
+        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_resources_for_location_request.vtl"))
+            .context(context)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
+        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
+        
+        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
+        Assertions.assertEquals("Query", resultMapRequest.get("operation").asText());
+        Assertions.assertEquals("locationid = :locationid", resultMapRequest.get("query").get("expression").asText());
+        Assertions.assertEquals(contextMap.get("arguments").get("locationid").asText(), resultMapRequest.get("query").get("expressionValues").get(":locationid").get("S").asText());
+        Assertions.assertEquals("locationidGSI", resultMapRequest.get("index").asText());
+
+        // Test response mapping response
+        EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_resources_for_location_response.vtl"))
+            .context(context)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
+        Assertions.assertNotNull(templateResponseResponse.evaluationResult());
+
+        JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        System.out.println("resultMap " + resultMapResponse);
+        int i=0;
+        for (JsonNode jsonNode : resultMapResponse) {
+            Assertions.assertEquals(contextMapItems.get(i).get("locationid").asText(), jsonNode.get("locationid").asText());
+            Assertions.assertEquals(contextMapItems.get(i).get("resourceid").asText(), jsonNode.get("resourceid").asText());
+            Assertions.assertEquals(contextMapItems.get(i).get("name").asText(), jsonNode.get("name").asText());
+            Assertions.assertEquals(contextMapItems.get(i).get("description").asText(), jsonNode.get("description").asText());
+            Assertions.assertEquals(contextMapItems.get(i).get("type").asText(), jsonNode.get("type").asText());
+            Assertions.assertEquals(contextMapItems.get(i).get("timestamp").asText(), jsonNode.get("timestamp").asText());
+            i++;
+        }
+    }
+
+    @Test
     public void testCreateBookingResolverWithBookingId() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode contextMap = mapper.readTree(bookingContext);
@@ -228,7 +318,75 @@ public class MockTest {
         // Test response mapping response
         EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
             .template(getTemplateToString("create_booking_request.vtl"))
-            .context(locationContext)
+            .context(bookingContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
+        Assertions.assertNotNull(templateResponseResponse.evaluationResult());
+
+        JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        for (JsonNode jsonNode : resultMapResponse) {
+            Assertions.assertEquals(contextMap.get("result").get(jsonNode.asText()), resultMapResponse.get(jsonNode.asText()));
+        }
+    }
+
+    @Test
+    public void testCreateBookingResolverWithoutBookingId() throws IOException {
+        String context = "{\n        \"arguments\":\n            {\n                \"resourceid\": \"abcdefghij\"\n            },\n        \"identity\":\n            {\n                \"sub\": \"123456-abcdeefg-7890\",\n                \"issuer\": \"\",\n                \"username\": \"johndoe\",\n                \"claims\": {},\n                \"sourceIp\": [\n                    \"x.x.x.x\"\n                ],\n                \"defaultAuthStrategy\": \"ALLOW\"\n            },\n        \"result\":\n            {\n                \"bookingid\": \"1234567890\",\n                \"resourceid\": \"abcdefghij\",\n                \"starttimeepochtime\": 1672578000,\n                \"userid\": \"123456-abcdeefg-7890\",\n                \"timestamp\": \"2023-01-01T00:00:00.000Z\"\n            }\n    }";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode contextMap = mapper.readTree(context);
+
+        // Test request mapping template
+        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("create_booking_request.vtl"))
+            .context(context)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
+        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
+        
+        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
+        
+        Assertions.assertEquals("PutItem", resultMapRequest.get("operation").asText());
+        Assertions.assertNotNull(resultMapRequest.get("key").get("bookingid").get("S"));
+        Assertions.assertEquals(contextMap.get("arguments").get("resourceid"), resultMapRequest.get("attributeValues").get("resourceid").get("S"));
+        Assertions.assertEquals(contextMap.get("identity").get("sub"), resultMapRequest.get("attributeValues").get("userid").get("S"));
+        Assertions.assertNotNull(resultMapRequest.get("attributeValues").get("timestamp").get("S"));
+
+        // Test response mapping response
+        EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("create_booking_request.vtl"))
+            .context(context)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
+        Assertions.assertNotNull(templateResponseResponse.evaluationResult());
+
+        JsonNode resultMapResponse = mapper.readTree(templateResponseResponse.evaluationResult());
+        for (JsonNode jsonNode : resultMapResponse) {
+            Assertions.assertEquals(contextMap.get("result").get(jsonNode.asText()), resultMapResponse.get(jsonNode.asText()));
+        }
+    }
+
+    @Test
+    public void testGetSingleBookingResolver() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode contextMap = mapper.readTree(bookingContext);
+
+        // Test request mapping template
+        EvaluateMappingTemplateRequest templateRequest = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_booking_request.vtl"))
+            .context(bookingContext)
+            .build();
+        EvaluateMappingTemplateResponse templateResponseRequest = appClient.evaluateMappingTemplate(templateRequest);
+        Assertions.assertNotNull(templateResponseRequest.evaluationResult());
+        
+        JsonNode resultMapRequest = mapper.readTree(templateResponseRequest.evaluationResult());
+        
+        Assertions.assertEquals("GetItem", resultMapRequest.get("operation").asText());
+        Assertions.assertEquals(contextMap.get("arguments").get("bookingid"), resultMapRequest.get("key").get("bookingid").get("S"));
+        
+        // Test response mapping response
+        EvaluateMappingTemplateRequest templateResponse = EvaluateMappingTemplateRequest.builder()
+            .template(getTemplateToString("get_booking_response.vtl"))
+            .context(bookingContext)
             .build();
         EvaluateMappingTemplateResponse templateResponseResponse = appClient.evaluateMappingTemplate(templateResponse);
         Assertions.assertNotNull(templateResponseResponse.evaluationResult());
@@ -244,7 +402,6 @@ public class MockTest {
         try {
             Path path = Paths.get(currentDir,"src","mapping_templates",location);
             String content = new String(Files.readAllBytes(path));
-            System.out.println("vtl > " + content);
             return content;
         } catch (IOException e) {
             throw e;
